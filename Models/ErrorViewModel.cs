8ca616ae400b3aa926f2cd49fbe3033f92e5125f
Valentine Masina <valentinemasina@gmail.com>
2021-06-22 17:08:46 +0200

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Github_Pipeline.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
